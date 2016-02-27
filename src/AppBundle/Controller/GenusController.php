b1a8a4172af543297127182a0b0369e71febb9e5
vorenus85 <janos.perge@gmail.com>
2016-02-27 23:06:54 +0100

    elso leckék

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        $templating = $this->container->get('templating');
+        $html = $templating->render('genus/show.html.twig', [
+            'name'=>$genusName
+        ]);
+
+        return new Response($html);
+    }
+}
