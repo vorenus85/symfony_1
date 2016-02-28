f5b7245b1927d74a78f9da8415883fd483332233
vorenus85 <janos.perge@gmail.com>
2016-02-28 09:37:59 +0100

    2. etap kövi lecke: http://knpuniversity.com/screencast/symfony/json-api#play

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -14,11 +14,15 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $templating = $this->container->get('templating');
-        $html = $templating->render('genus/show.html.twig', [
-            'name'=>$genusName
-        ]);
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
 
-        return new Response($html);
+        return $this->render('genus/show.html.twig', [
+            'name'=>$genusName,
+            'notes' => $notes
+        ]);
     }
 }
